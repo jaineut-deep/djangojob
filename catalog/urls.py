f81614aef6892a28a5d80b1cb69f92a45468f9b9
Артем Бичеров <jaineut@gmail.com>
2026-03-24 11:59:12 +0300

    Add controllers and set their routes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
 
+urlpatterns = [
+    path("", views.pass_home, name="pass_home"),
+    path("contacts/", views.pass_contacts, name="pass_contacts")
 ]
