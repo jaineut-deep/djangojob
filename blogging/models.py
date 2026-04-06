e7acfc04f4dbb1ff46832af8981237e154869ea0
Артем Бичеров <jaineut@gmail.com>
2026-04-06 04:23:38 +0300

    Add Article model and base CRUD controllers

--- blogging/models.py
+++ blogging/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержимое")
+    preview = models.ImageField(upload_to="images/", verbose_name="Превью")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_published = models.BooleanField(default=False, verbose_name="Признак публикации")
+    view_count = models.IntegerField(default=0, verbose_name="Количество просмотров")
+
+    def __str__(self):
+        return f"{self.title} / {self.created_at}"
+
+    class Meta:
+        verbose_name = "статья"
+        verbose_name_plural = "статьи"
+        ordering = ["title"]
--- blogging/views.py
+++ blogging/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from blogging.models import Article
 
-# Create your views here.
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ["title", "content", "preview", "is_published"]
+    template_name = "blogging/article_form.html"
+    success_url = reverse_lazy("blogging:article_list")
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "blogging/article_list.html"
+    context_object_name = "articles"
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "blogging/article_details.html"
+    context_object_name = "article"
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ["title", "content", "preview", "is_published"]
+    template_name = "blogging/article_form.html"
+    success_url = reverse_lazy("blogging:article_list")
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "blogging/article_confirm_delete.html"
+    success_url = reverse_lazy("blogging:article_list")
