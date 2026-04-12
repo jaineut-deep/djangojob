7f16e2463c9c2777b000dc0871e4cd213816e5cd
Артем Бичеров <jaineut@gmail.com>
2026-04-12 16:01:46 +0300

    Register controllers and add success url for update view

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import ContactsTemplateView, ProductListView, ProductDetailView
+from .views import (ContactsTemplateView, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView,
+                    ProductDeleteView)
 
 app_name = "catalog"
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="pass_home"),
     path("contacts/", ContactsTemplateView.as_view(), name="pass_contacts"),
+    path("product_create/", ProductCreateView.as_view(), name="product_create"),
+    path("product_update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
+    path("product_delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
     path("product_details/<int:pk>/", ProductDetailView.as_view(), name="pass_product_details"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView, TemplateView
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.http import HttpResponse
 from catalog.models import Product
 from .forms import ProductForm
@@ -42,7 +42,9 @@ class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     template_name = "catalog/product_form.html"
-    success_url = reverse_lazy("catalog:pass_product_details")
+
+    def get_success_url(self):
+        return reverse("catalog:pass_product_details", kwargs={"pk": self.object.pk})
 
 
 class ProductDeleteView(DeleteView):
