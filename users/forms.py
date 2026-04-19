2d67fa2cf5605e563f299a20f027243476e90854
Артем Бичеров <jaineut@gmail.com>
2026-04-20 01:03:25 +0300

    Add register form with respective fields

---
+++ users/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    phone_number = forms.CharField(max_length=15, required=False,
+                                   help_text="Необязательное поле. Введите ваш номер телефона")
+    country = forms.CharField(max_length=35, required=False,
+                                   help_text="Необязательное поле. Введите ваш страну")
+    username = forms.CharField(max_length=50, required=True)
+    usable_password = None
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ("email", "username", "first_name", "last_name", "phone_number", "country", "password1", "password2")
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get("phone_number")
+        if phone_number and not phone_number.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры.")
+        return phone_number
