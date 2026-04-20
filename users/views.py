0e26991a11879c4178d9034f51ce347c24a2a418
Артем Бичеров <jaineut@gmail.com>
2026-04-20 04:25:37 +0300

    Add register view with form valid method and mailing

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.core.mail import send_mail
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from config.settings import EMAIL_HOST_USER
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "register.html"
+    success_url = reverse_lazy("catalog:pass_home")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = "Добро пожаловать в наш сервис"
+        message = "Спасибо, что зарегистрировались в нашем сервисе!"
+        from_email = EMAIL_HOST_USER
+        recipient_list = [user_email]
+        send_mail(subject, message, from_email, recipient_list)
