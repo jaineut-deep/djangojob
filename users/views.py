afd8cd28b4a0d1697cd8c813fa01afc8ac435ba7
Артем Бичеров <jaineut@gmail.com>
2026-04-20 04:56:26 +0300

    Add urls routes in app users

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from .forms import CustomUserCreationForm
 
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
-    template_name = "register.html"
+    template_name = "users/register.html"
     success_url = reverse_lazy("catalog:pass_home")
 
     def form_valid(self, form):
