1cd1222a0eb4794fdecc770bc68fda551de008b4
Артем Бичеров <jaineut@gmail.com>
2026-04-27 20:19:08 +0300

    Add owner field, make and apply migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -24,9 +25,15 @@ class Product(models.Model):
     is_published = models.BooleanField(default=False, verbose_name="Статус публикации")
     created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateField(auto_now=True, verbose_name="Дата последнего изменения")
+    owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Владелец")
 
     def __str__(self):
         return f"{self.name} - {self.category}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.owner = kwargs.pop("user", None)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "продукт"
