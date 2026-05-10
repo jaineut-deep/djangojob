f1a068d15e7f7afaa72ede615e2e168a846b05ee
Артем Бичеров <jaineut@gmail.com>
2026-05-11 00:39:11 +0300

    Add small fixes in env example and settings files

--- config/settings.py
+++ config/settings.py
@@ -156,10 +156,12 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # caching settings
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://{os.getenv("REDIS_HOST", "127.0.0.1")}:{os.getenv("REDIS_PORT", "6379")}/'
-                    '{os.getenv("REDIS_DB", "1")}',
+CACHE_ENABLED = True if os.getenv("CACHE_ENABLED") == "True" else False
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://{os.getenv("REDIS_HOST", "127.0.0.1")}:{os.getenv("REDIS_PORT", "6379")}/'
+                        '{os.getenv("REDIS_DB", "1")}',
+        }
     }
-}
